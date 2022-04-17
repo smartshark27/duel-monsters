bc5f6c182532a8a45b13dc724e116817889fbc7e
smartshark27 <thomassmart97@gmail.com>
2022-04-17 20:11:08 +1000

    Lift game running to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+import { Phase } from "./enums";
 import Deck from "./modules/Deck";
 import Duel from "./modules/Duel";
 import Player from "./modules/Player";
+import Util from "./util/Util";
 
 const player1 = new Player("Tom");
 const player2 = new Player("Thomas");
@@ -11,6 +13,24 @@ const deck2 = new Deck(player2, "dragon_rage");
 player1.setDeck(deck1);
 player2.setDeck(deck2);
 
-global.DUEL = new Duel([player1, player2]);
-global.DUEL.init();
-global.DUEL.start();
+const duel = new Duel([player1, player2]);
+global.DUEL = duel
+
+autoRun(duel);
+duel.printResults();
+
+function autoRun(duel: Duel) {
+  let actions = duel.getActions();
+  while (duel.running) {
+    while (actions.length > 0) {
+      const action = Util.getRandomItemFromArray(actions);
+      duel.performAction(action);
+      if (!duel.running) break;
+      actions = duel.getActions();
+    }
+    if (!duel.running) break;
+    if (duel.phase === Phase.End) duel.switchTurns();
+    duel.startNextPhase();
+    actions = duel.getActivePlayer().getActions();
+  }
+}
--- src/modules/Duel.ts
+++ src/modules/Duel.ts
@@ -1,6 +1,5 @@
 import { Phase, PHASE_ENUM_LENGTH } from "../enums";
 import LoggerFactory from "../util/LoggerFactory";
-import Util from "../util/Util";
 import Action from "./actions/Action";
 import Player from "./Player";
 
@@ -15,50 +14,29 @@ export default class Duel {
   constructor(private players: Player[]) {
     Duel.logger.info("Creating duel");
     this.activePlayer = players[this.activePlayerIndex];
-  }
-
-  init() {
     this.players.forEach((player) => player.init());
-  }
-
-  start() {
-    this.running = true;
     this.activePlayer = this.getActivePlayer();
     this.activePlayer.havingTurn = true;
-    let actions = this.activePlayer.getActions();
-    while (this.running) {
-      while (actions.length > 0) {
-        this.performRandomAction(actions);
-        if (!this.running) break;
-        actions = this.activePlayer.getActions();
-      }
-      if (!this.running) break;
-      if (this.phase === Phase.End) this.switchTurns();
-      this.startNextPhase();
-      actions = this.activePlayer.getActions();
-    }
-    this.printResults();
+    this.running = true;
   }
 
-  end(winner: Player) {
-    this.winner = winner;
-    this.running = false;
+  getActions(): Action[] {
+    return this.activePlayer.getActions();
   }
 
-  getActivePlayer() {
-    return this.players[this.activePlayerIndex];
-  }
-
-  getInactivePlayer() {
-    return this.players[this.getInactivePlayerIndex()];
-  }
-
-  private performRandomAction(actions: Action[]) {
-    const action = Util.getRandomItemFromArray(actions);
+  performAction(action: Action) {
     action.perform();
   }
 
-  private startNextPhase() {
+  switchTurns() {
+    this.activePlayer.havingTurn = false;
+    this.activePlayerIndex = this.getInactivePlayerIndex();
+    this.activePlayer = this.players[this.activePlayerIndex];
+    this.activePlayer.havingTurn = true;
+    Duel.logger.info(`Switched to player ${this.activePlayer.name}`);
+  }
+
+  startNextPhase() {
     this.phase = (this.phase + 1) % PHASE_ENUM_LENGTH;
     if (this.phase == Phase.Draw) {
       this.activePlayer.startDrawPhase();
@@ -73,23 +51,28 @@ export default class Duel {
     }
   }
 
-  private switchTurns() {
-    this.activePlayer.havingTurn = false;
-    this.activePlayerIndex = this.getInactivePlayerIndex();
-    this.activePlayer = this.players[this.activePlayerIndex];
-    this.activePlayer.havingTurn = true;
-    Duel.logger.info(`Switched to player ${this.activePlayer.name}`);
+  end(winner: Player) {
+    this.winner = winner;
+    this.running = false;
   }
 
-  private getInactivePlayerIndex() {
-    return this.activePlayerIndex == 0 ? 1 : 0;
-  }
-
-  private printResults() {
+  printResults() {
     Duel.logger.info("Game has ended");
     const winner = this.winner as Player;
     const loser = this.players.find((player) => player !== winner) as Player;
     Duel.logger.info(`The winner is ${winner.name}`);
     Duel.logger.info(`The loser is ${loser.name}`);
+  }
+
+  getActivePlayer() {
+    return this.players[this.activePlayerIndex];
+  }
+
+  getInactivePlayer() {
+    return this.players[this.getInactivePlayerIndex()];
+  }
+
+  private getInactivePlayerIndex() {
+    return this.activePlayerIndex == 0 ? 1 : 0;
   }
 }
